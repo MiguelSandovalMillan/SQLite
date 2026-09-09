e9ea00eb2c631c9d130e9c989995f3bae205bf8d
MiguelSandovalMillan <2408819j@umich.mx>
2026-09-09 11:55:02 -0600

    09/09

---
+++ IN.sql
@@ -0,0 +1,18 @@
+CREATE TABLE countries (
+    location_x REAL NOT NULL,
+    location_y REAL NOT NULL,
+    country TEXT NOT NULL
+);
+
+INSERT INTO countries (location_x, location_y, country) VALUES
+(53.0, 27.53, 'Belarus'),
+(57.18, 24.96, 'Latvia'),
+(3.0, 134.25, 'Australia'),
+(4.0, 55.85, 'Oman'),
+(5.0, 45.45, 'Somalia'),
+(6.0, -85.32, 'Somalia'),
+(7.0, 90.5, 'Nicaragua'),
+(8.0, 139.61, 'Bhutan'),
+(9.0, -14.47, 'Japan'),
+(10.0, 10.0, 'Senegal'),
+(39.39, -3.21, 'Spain');
