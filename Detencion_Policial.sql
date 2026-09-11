cc5acb7c249e3eb96a42c8c01b25c3b3d83af008
MiguelSandovalMillan <2408819j@umich.mx>
2026-09-11 14:39:27 -0600

    11/09

---
+++ Detencion_Policial.sql
@@ -0,0 +1,40 @@
+CREATE TABLE police_report(
+    name TEXT PRIMARY KEY,
+    report TEXT NOT NULL,
+    map TEXT NOT NULL,
+    severe_score INTEGER NOT NULL
+);
+
+
+INSERT INTO police_report(name, report, map, severe_score) VALUES
+('Domingos Holden','VYyPJw','Lockinge',2),
+('Isabel Enid','','Findochty',7),
+('Tadgán Musa','FgDqud',"Kara's Vale",10),
+('Filip Baxter','D1rJqH','Kirkwall',3),
+('Edorta Elias','pQ53RC','Dewsbury',9),
+('Kəmal Davy','BsI86j',"Kara's Vale",2),
+('Isokrates Bituin','','Findochty',5),
+('Kassy Ramirus','Pkjv7J','Kirkwall',7),
+('Doris Blessing','0kJXq1','Dalmerlington',6),
+('Nedeljka Ganesh','EVe9ha','Kara's Vale',8),
+('Karlene Timotheus','2HUBHz','Dewsbury',7),
+('Emerens Raman','PAGdHl','Findochty',1),
+('Mijo Ambrosios','','Luton',5),
+('Karlene Jairus','4Z63Q5','Dalmerlington',8),
+('Khordad Peter','','Findochty',2),
+('Biago Mai','','Findochty',9),
+('Liwen Sigiward','B51ETs','Mansfield',3),
+('Svante Mona','DC0kGh','Caerleon',5),
+('Kshitija Ladislav','cSA0hA','Caerleon',2),
+('Ha-Eun Tatiana','YNYhHV',"Kara's Vale",4);
+
+
+
+DROP TABLE police_report
+
+
+SELECT * FROM police_report
+
+-- Karlene Timotheus
+
+-- Svante Mona
